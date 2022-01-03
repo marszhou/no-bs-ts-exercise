30eef1357f4be1d31d9a8b1e3688b7d98e27643a
matt zhou <matt@cruelyouth.com>
2022-01-03 20:24:12 +0800

    generic component

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,60 @@
-import React, {
-  useCallback,
-  useRef,
-} from 'react'
-import {useTodos} from './useTodos'
+import React, { useCallback, useRef } from 'react'
+import { useTodos } from './useTodos'
 import './App.css'
+import { render } from '@testing-library/react'
 
-const Heading: React.FunctionComponent<{ title: string }> = ({ title }) => (
-  <h2>{title}</h2>
-)
+const Heading: React.FunctionComponent<{
+  title?: string
+  children?: React.ReactNode
+}> = ({ title, children }) => <h2>{title ?? children}</h2>
+
+function UL<T>({
+  items,
+  render,
+  itemClick
+}: React.DetailedHTMLProps<
+  React.HTMLAttributes<HTMLUListElement>,
+  HTMLUListElement
+> & {
+  items: T[]
+  render: (item: T) => React.ReactNode,
+  itemClick?: (item: T) => void
+}) {
+  return (
+    <ul>
+      {items.map((item, index) => (
+        <li key={index} onClick={() => itemClick?.(item)}>{render(item)}</li>
+      ))}
+    </ul>
+  )
+}
 
 function App() {
-  const {todos, addTodo, removeTodo} = useTodos([{id: 0, text: 'sleeping', done: false}])
+  const { todos, addTodo, removeTodo } = useTodos([
+    { id: 0, text: 'sleeping', done: false },
+  ])
   const newTodoRef = useRef<HTMLInputElement>(null)
   const onAddTodo = useCallback(() => {
     if (newTodoRef.current) {
-      addTodo(newTodoRef.current.value )
+      addTodo(newTodoRef.current.value)
       newTodoRef.current.value = ''
     }
   }, [])
 
-
   return (
     <div>
       <Heading title="Todos" />
-      {todos.map((todo) => (
-        <div key={todo.id}>
-          {todo.text}
-          <button onClick={() => removeTodo(todo.id)}>
-            remove
-          </button>
-        </div>
-      ))}
+      <UL
+        items={todos}
+        itemClick={(item) => alert(item.id)}
+        render={(todo) => (
+          <>
+            {todo.text}
+            <button onClick={() => removeTodo(todo.id)}>remove</button>
+          </>
+        )}
+      />
+
       <div>
         <input type="text" ref={newTodoRef} />
         <button onClick={onAddTodo}>add</button>
